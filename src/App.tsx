7405bc97dae48eba6d6ee66c3c6595f39e1c1632
Ivan Melentev <ivan.saxalin@mail.ru>
2020-09-15 00:51:21 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import View from '@vkontakte/vkui/dist/components/View/View';
 import '@vkontakte/vkui/dist/vkui.css';
 import Home from './panels/Home';
 import Persik from './panels/Persik';
-import { Page, Router, useLocation, VIEW_MAIN } from '@happysanta/router';
-import { PageEnum } from './enums/PageEnum';
+import { useLocation, VIEW_MAIN } from '@happysanta/router';
 import { PanelEnum } from './enums/PanelEnum';
 
 
